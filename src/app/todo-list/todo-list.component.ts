38faffe499b5cb275fa64a3fe995dfaf35038a08
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-07 16:44:45 +0200

    1-add header markup

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
 })
-export class TodoListComponent {
-
-}
+export class TodoListComponent {}
