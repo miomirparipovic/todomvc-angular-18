e53d0adf672441e0daba17ca98c7fed381dea0d5
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-09 13:25:27 +0200

    1-add service, interface, console.log value of input

--- src/app/todo-list/components/header/header.component.ts
+++ src/app/todo-list/components/header/header.component.ts
@@ -5,8 +5,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  text: string = '';
 
+  addText(event: KeyboardEvent): void {
+    const target = event.target as HTMLInputElement;
+
+    if (event.key == 'Enter' && this.text) {
+      console.log('enter pressed', this.text);
+      this.text = '';
+      return;
+    }
+
+    if (target) {
+      this.text = target.value;
+      console.log('text', this.text);
+      return;
+    }
+  }
 }
