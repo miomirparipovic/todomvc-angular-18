9dd5f6a3eb97412c97c2e09fe03690757a6cecba
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-11 15:32:06 +0200

    1-add items left in footer

--- src/app/todos/components/footer/footer.component.ts
+++ src/app/todos/components/footer/footer.component.ts
@@ -9,4 +9,19 @@ import { Component, Input } from '@angular/core';
 })
 export class FooterComponent {
   @Input() areThereAnyTodos!: boolean | null;
+  @Input() numberOfVisibleTodos?: number;
+
+  itemMessage(numberOfVisibleTodos: number | undefined): string | undefined {
+    if (numberOfVisibleTodos == undefined) {
+      return;
+    }
+
+    let word = 'item';
+
+    if (this.numberOfVisibleTodos != 1) {
+      word += 's';
+    }
+
+    return word + ' ' + 'left';
+  }
 }
