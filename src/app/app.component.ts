fa38e751865336eb8f88caf8c126054255e0b11b
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-15 16:42:50 +0200

    Set to fresh, initial, empty app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  constructor(private _router: Router) {
-    // initial navigation
-    _router.navigate(['/all']);
-  }
-}
+export class AppComponent {}
