a7188d70cfc8ad28d1a3b3d7f2027dea54ebd095
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-16 16:35:14 +0200

    2-Add header markup, addTodo method, 'keyup' in header

--- src/app/todos/components/todo-header/todo-header.component.ts
+++ src/app/todos/components/todo-header/todo-header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-header',
   standalone: true,
   imports: [],
   templateUrl: './todo-header.component.html',
-  styleUrl: './todo-header.component.css'
+  styleUrl: './todo-header.component.css',
 })
 export class TodoHeaderComponent {
+  todoText: string = '';
+  newTodo = output<string>();
 
+  onInputTodo(event: KeyboardEvent): void {
+    const target = event.target as HTMLInputElement;
+
+    if (event.key == 'Enter') {
+      this.newTodo.emit(this.todoText);
+      this.todoText = '';
+      return;
+    }
+
+    if (target) {
+      this.todoText = target.value;
+      return;
+    }
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
 import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
 import { TodoMainComponent } from './components/todo-main/todo-main.component';
+import { TodosService } from './services/todos.service';
 
 @Component({
   selector: 'app-todos',
@@ -10,4 +11,16 @@ import { TodoMainComponent } from './components/todo-main/todo-main.component';
   imports: [TodoHeaderComponent, TodoFooterComponent, TodoMainComponent],
   styleUrl: './todos.component.css',
 })
-export class TodosComponent {}
+export class TodosComponent {
+  private _todosService: TodosService;
+
+  constructor(_todosService: TodosService) {
+    this._todosService = _todosService;
+  }
+
+  handleNewTodo(event: string): void {
+    // console.log('event in todos', event);
+
+    this._todosService.addTodo(event);
+  }
+}
