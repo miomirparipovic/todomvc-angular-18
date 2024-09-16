89252eba7a9221ed83e4a1895be662b00d5f0738
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-16 12:37:35 +0200

    2-Add interface, test component working in todos

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
+import { TodoFooterComponent } from './components/todo-footer/todo-footer.component';
+import { TodoMainComponent } from './components/todo-main/todo-main.component';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   standalone: true,
+  imports: [TodoHeaderComponent, TodoFooterComponent, TodoMainComponent],
   styleUrl: './todos.component.css',
 })
 export class TodosComponent {}
