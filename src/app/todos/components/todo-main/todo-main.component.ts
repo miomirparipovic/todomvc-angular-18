85af40a9abe6d41264d262ad2e1505dc9ff5afbf
Miomir Paripovic <deploy.friday@gmail.com>
2024-09-16 11:47:51 +0200

    2-Add four components: todo-header, todo-main, todo-item, and todo-footer

---
+++ src/app/todos/components/todo-main/todo-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-main.component.html',
+  styleUrl: './todo-main.component.css'
+})
+export class TodoMainComponent {
+
+}
